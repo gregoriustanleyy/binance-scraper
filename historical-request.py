1e2edb6f47dc88027965ae0a3db8d2d852807733
gregoriustanleyy <gtejakus@purdue.edu>
2022-11-08 22:02:16 -0500

    todo: find 1 other calculation for volatility

--- historical-request.py
+++ historical-request.py
@@ -1,34 +1,40 @@
-from binance.client import Client
-import numpy as np
-import requests
-import json
-import pandas as pd
 import datetime as dt
+import pandas as pd
+import json
+import requests
+import numpy as np
+from binance.client import Client
+import time
+import warnings
+warnings.filterwarnings("ignore")
+start_time = time.time()
+np.set_printoptions(threshold=np.inf)
 
 
-#get token individual data
+def to_datetime(timestamp):
+    return dt.datetime.fromtimestamp(float(timestamp)/1000.0)
+
+# get token individual data
+# cols: datetime, open, high, close, low, volume(usdt), num_trades
 def get_token_data(symbol: str, interval: str):
     url = 'https://api.binance.com/api/v3/klines'
     par = {'symbol': symbol, 'interval': interval, 'limit': 1000}
-    data = pd.DataFrame(json.loads(requests.get(url, params= par).text))
-    data.columns = ['datetime', 'open', 'high', 'low', 'close', 'base_volume','close_time', 'quote_volume', 'num_trades','taker_base_vol', 'taker_quote_vol', 'ignore']
-    data.index = [dt.datetime.fromtimestamp(x/1000.0) for x in data.datetime]
-    data = data.drop(['datetime', "close_time", 'taker_base_vol', 'taker_quote_vol', "ignore"], axis = 1)
-    data=data.astype(float)
-    return data
-    
-#get all spot pairs
+    temp = json.loads(requests.get(url, params=par).text)
+    arr = np.delete(np.array(temp), [5, 6, 9 ,10, 11], 1)
+    arr[:, 0] = np.array(list(map(to_datetime, arr[:,0])))
+    arr[:, 1:6] = arr[:,1:6].astype(float).round(2)
+    return arr
+
+#get all SPOT pair
 def all_usdt_spot_pair():
     BASE_URL = 'https://api.binance.com'
     resp = requests.get(BASE_URL+'/api/v1/ticker/allBookTickers')
     ticker_list = json.loads(resp.content)
-    symbols = []
-    for ticker in ticker_list:
-        if str(ticker['symbol'][-4:])=='USDT':
-            symbols.append(ticker['symbol'])
+    symbols = [i["symbol"]
+               for i in ticker_list if str(i['symbol'][-4:]) == 'USDT']
     return symbols
 
-#get all futures pair
+# get all FUTURES pair
 def all_usdt_futures_pair():
     client = Client(api_key, api_secret)
     info = client.futures_exchange_info()
@@ -38,20 +44,59 @@ def all_usdt_futures_pair():
     symbols = [str(i + "USDT") for i in set(temp)]
     return symbols
 
-#calculate volatility
-def volatility(df: pd.DataFrame):
-    df["pct_oc"] = (df["close"] - df["open"])/df["close"]
-    df["pct_hl"] = (df["high"] - df["low"])/df["close"]
-    df["mean"] = df[["open", "high", "low", "close"]].mean(axis = 1)
-    df["vwap"] = (np.cumsum(df["mean"] * df["quote_volume"]) / np.cumsum(df["quote_volume"]))
-    df["std"] = df[["open", "high", "low", "close"]].std(axis = 1)
-    
-    #TODO: find the best way to calculate
-    print(df)
-    return df["std"].std()/np.sqrt(len(df)), df["pct_oc"].std(), df["pct_hl"].std()
+# calculate volatility
+# cols: datetime, open, high, close, low, volume(usdt), num_trades, pct_oc, pct_hl, avg_price, vwap
+def calculate(data: np.ndarray):
+    row = np.shape(data)[0]
+    open = data[:,1].astype(float)
+    high = data[:,2].astype(float)
+    close = data[:,3].astype(float)
+    low = data[:,4].astype(float)
+    vol = data[:,5].astype(float)
 
-#main function
-api_key = 'KVcT086rW4f2BM9or3bE4cgqC11MnvvKxQryeKx0n3HmkTfBGQwWAlVkcu1evttb' 
+    pct_oh = ((close - open) / close * 100).round(2).reshape(row,1)
+    pct_hl = ((high - low) / low * 100).round(2).reshape(row,1)
+    avg_price = np.mean(data[:,1:5].astype(float), axis = 1).round(2).reshape(row,1)
+    vwap = (np.cumsum((high + low + close) / 3 * vol) / np.cumsum(vol)).round(2).reshape(row, 1)
+    data = np.concatenate([data, pct_oh, pct_hl, avg_price, vwap], axis=1)
+
+    last_price = float(data[-1,3])
+    pct_change = (close[-1] - open[0]) / close[-1] * 100
+    avg_trade = np.mean(data[:,6].astype(float))
+    avg_vol = np.mean(vol)
+    v_t = avg_vol/avg_trade
+
+    vlt = np.round(np.std(vwap/np.mean(vwap))*100, 2)
+    # TODO: find the best way to calculate
+    # print(np.std(df["close"])/df["close"].mean()*100, np.std(df["avg_price"])/df["avg_price"].mean()*100, )
+    return data[0,0], last_price, pct_change, avg_trade, avg_vol, v_t, vlt
+
+# main function
+api_key = 'KVcT086rW4f2BM9or3bE4cgqC11MnvvKxQryeKx0n3HmkTfBGQwWAlVkcu1evttb'
 api_secret = '1goM6odH4ZQOXe5anVnFy5AD6HlXF8jfoeNUNWfNJbQo4dClyesAOdTmQstREh3w'
-interval = ['1s', '1m', '5m', '15m', '1h', '1d']
-print(volatility(get_token_data("BTCUSDT", "1m")))
+interval = ['1s', '1m', '5m', '15m', '1h']
+
+spot = all_usdt_spot_pair()
+futures = all_usdt_futures_pair()
+futures_t = "\t".join(futures)
+symbols = [i for i in spot if i in futures_t]
+
+final_df = pd.DataFrame(columns=["DateTime", "Token", "TimeFrame", "LastPrice", "% Change",
+                        "Avg Num Trade", "Avg Volume", "Volume/Trade", "Volatility"])
+count = len(symbols)
+for i in symbols:
+    count -= 1
+    print(str(count) + " Left")
+    for j in interval:
+        print(i, j)
+        run_time = time.time()
+        data = get_token_data(symbol=i, interval=j)                                  
+        date, last_price, pct_change, avg_trade, avg_vol, v_t, vlt = calculate(data)
+
+        final_df = final_df.append({"DateTime": date, "Token": i, "TimeFrame": j, "LastPrice": last_price, "% Change": pct_change, "Avg Num Trade": avg_trade,
+                                   "Avg Volume": avg_vol, "Volume/Trade": v_t, "Volatility": str(str(vlt)+"%")}, ignore_index=True)
+        print("--- %s seconds ---" % np.round((time.time() - run_time), 2))  
+final_df.to_csv("Volatility.csv")
+print("--- %s seconds ---" % np.round((time.time() - start_time), 2))
+
+print(get_token_data("BTCUSDT", "1h"))
